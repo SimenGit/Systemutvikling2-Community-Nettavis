4f4725f79eff47522d1b8c7fd2cef95d933bf37d
SimenGit <35735837+SimenGit@users.noreply.github.com>
2018-11-02 13:26:15 +0100

    Lagt til body-parsing og error handling.

--- api/routes/products.js
+++ api/routes/products.js
@@ -8,8 +8,39 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    }
     res.status(200).json({
-        message: 'Handling POST requests to /products'
+        message: 'Handling POST requests to /products',
+        createdProduct: product
+    });
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'You discovered the special ID',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
+
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated product!'
+    });
+});
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted product!'
     });
 });
 
--- app.js
+++ app.js
@@ -1,8 +1,40 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+//middleware
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
 
+//bruker morgan før den handler requests
+app.use(morgan('dev'));
+
+//false for å bare handle url-encoded data
+app.use(bodyParser.urlencoded({extended: false}));
+//extract json data og gjør det lettere leselig
+app.use(bodyParser.json());
+
+//routes that handle requests
 app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+//custom 404 error handler
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+//returnerer error som json
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
