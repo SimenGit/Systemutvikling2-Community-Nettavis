f741a2009195db9eb975629276cdfc6808b0e18a
Simen <simenten@stud.ntnu.no>
2018-11-09 13:30:24 +0100

    update.

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,14 @@ import React from 'react';
 import { Component } from 'react-simplified';
 import FrontPage from './FrontPage';
 import SearchField from './SearchField';
-
+import FrontPageTop from './FrontPageTop';
 
 const root = document.getElementById('root');
-if (root) ReactDOM.render(<div> <FrontPage/> <SearchField/> </div>, root);
+if (root)
+  ReactDOM.render(
+    <div>
+      {' '}
+      <FrontPage /> <SearchField /> <FrontPageTop/>{' '}
+    </div>,
+    root
+  );
